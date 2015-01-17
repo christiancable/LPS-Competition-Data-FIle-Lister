f121f0f98698039bb7ef31cb81e03758749116ee
Christian Cable <christiancable@gmail.com>
2015-01-17 17:09:50 +0000

    Added length element to the JSON to show the size of files in bytes

--- index.php
+++ index.php
@@ -13,13 +13,14 @@ foreach ($files as $fileName) {
     $zipFiles[] = array(
         'url' => $urlPath . DIRECTORY_SEPARATOR . $fileName,
         'timestamp' => filemtime($fileName),
+        'length' => filesize($fileName),
     );
     
 }
 
 $competitionFiles = array();
 $competitionFiles['title'] = 'LPS Competition Manager Data';
-$competitionFiles['version'] = '0.1';
+$competitionFiles['version'] = '0.2';
 $competitionFiles['files'] = $zipFiles;
 
 $json = json_encode($competitionFiles, JSON_UNESCAPED_SLASHES);
