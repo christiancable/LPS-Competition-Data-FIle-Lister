e588f616550597c53002639b6a2df8c6b0994e8e
Christian Cable <christiancable@gmail.com>
2015-02-21 12:10:24 +0000

    added relativePath to the JSON format, moved up the version number of the format to 0.3

--- index.php
+++ index.php
@@ -1,27 +1,48 @@
 <?php
+/* JSON file listing for LPS Competition and Resizer  
+    christiancable@gmail.com - 2015
+*/
 
-$filePath = 'files';
 $filePattern = '*.*';
-$urlPath = 'http://www.lancasterphotographicsociety.org.uk/tools/competition';
+$currentScript = basename(__FILE__);
+$currentPath = getcwd();
+$relativePathName = basename(getcwd());
+$urlRoot = 'http://www.lancasterphotographicsociety.org.uk/tools/competition';
+$fileRoot = '/home/virtual/christiancable/lancasterphotographicsociety.org.uk/www/tools/competition';
 
-$files = glob($filePath . DIRECTORY_SEPARATOR . $filePattern);
+if ($fileRoot !== $currentPath) {
+    $relativePathName = str_replace($fileRoot . '/', '', $currentPath);
+} else {
+    $relativePathName = '';
+}
 
+$files = glob($currentPath . DIRECTORY_SEPARATOR . $filePattern);
 $zipFiles = array();
 
 
 foreach ($files as $fileName) {
-    $zipFiles[] = array(
-        'url' => $urlPath . DIRECTORY_SEPARATOR . $fileName,
-        'timestamp' => filemtime($fileName),
-        'length' => filesize($fileName),
-    );
-    
+    $currentFileName = basename($fileName);
+    if ($currentFileName !== $currentScript) {
+        if ($relativePathName === '') {
+            $currentFileURL = $urlRoot . DIRECTORY_SEPARATOR . $currentFileName;
+        } else {
+            $currentFileURL = $urlRoot . DIRECTORY_SEPARATOR . $relativePathName . DIRECTORY_SEPARATOR . $currentFileName;
+        }
+
+        $zipFiles[] = array(
+            'url' => $currentFileURL,
+            'timestamp' => filemtime($fileName),
+            'length' => filesize($fileName),
+        );
+    }
 }
 
 $competitionFiles = array();
 $competitionFiles['title'] = 'LPS Competition Manager Data';
-$competitionFiles['version'] = '0.2';
+$competitionFiles['version'] = '0.3';
 $competitionFiles['files'] = $zipFiles;
+$competitionFiles['relativePathName'] = "$relativePathName";
 
 $json = json_encode($competitionFiles, JSON_UNESCAPED_SLASHES);
+
 echo $json;
